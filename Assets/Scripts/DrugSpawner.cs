58acc894a1ab159143b232bf9d3c5bc9405c7b53
Jan Milosch <jan.milosch@uni-bayreuth.de>
2020-05-02 12:01:17 +0200

    Add prefab folder, Level 1 Scene, drug prefab and drug spawner script + prefab

---
+++ Assets/Scripts/DrugSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrugSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject drugPrefab1;
+    private Vector3 clickPosition;
+
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            SpawnDrug();
+        }
+    }
+
+    public void SpawnDrug() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // create a plane at 0,0,0 whose normal points to +Y:
+        Plane hPlane = new Plane(Vector3.up, Vector3.zero);
+        // Plane.Raycast stores the distance from ray.origin to the hit point in this variable:
+        float distance = 0;
+        // if the ray hits the plane...
+        if (hPlane.Raycast(ray, out distance)) {
+            // get the hit point:
+            clickPosition = ray.GetPoint(distance);
+            clickPosition.y = 5;
+        };
+
+        Instantiate(drugPrefab1, clickPosition, Quaternion.identity);
+    }
+
+}
